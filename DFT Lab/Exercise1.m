a6d3aad2d830c95d5cef06bace9a2484ce4d7b27
Alex Luisi <al3515@eews104a-015.ic.ac.uk>
2016-11-04 11:16:02 +0000

    Exercises Complete

--- DFT Lab/Exercise1.m
+++ DFT Lab/Exercise1.m
@@ -1,9 +1,12 @@
 %fsamp = 25600;
 %fsig = 1000;
-y = sinegen(25600, 1000, 127);
 %tsamp = 1/fsamp;
 %t = 0 : tsamp : 127 * tsamp;
 %y = sin(2*pi*fsig*t);
+% all code commented out has been replaced by the 
+% function sinegen with no loss of functionality
+
+y = sinegen(25600, 1000, 127);
 plot(y);
 grid;
 title('Simple sine wave');
